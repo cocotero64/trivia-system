475e056b4fecd90227837f90cd1466119afdb12d
Aurora <emanueldelacerda04@gmail.com>
2026-03-13 18:04:19 -0600

    fix port for deployment

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ app.post("/answer", (req, res) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
